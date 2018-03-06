53dba47177a95347d7b407a0a57f00dff2352438
NedSheynji13 <richardstrabl@gmail.com>
2018-03-06 15:18:35 +0100

    Added the Spirit(Blob,whateverthefuck) his appearance and his Movement

---
+++ Assets/Scripts/CharacterWorldMovement/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody2D physix;
+    private Vector2 Physix2Move;
+    [HideInInspector]
+    public float speed = 10;
+
+    // Use this for initialization
+    void Start()
+    {
+        physix = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Physix2Move.x = Input.GetAxis("Horizontal") * speed;
+        Physix2Move.y = physix.velocity.y;
+        physix.velocity = Physix2Move;
+    }
+}
