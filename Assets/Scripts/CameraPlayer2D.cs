babb76dfe55c53bec564e8a1f50abcb63b29642e
UncoveredDB <dominik.schraffl@hotmail.com>
2018-03-26 13:16:41 +0200

    Clamp Funktion zur Kamera hinzugefügt, Clamppositionen, lerpspeed und Kamera Offset kann vom Editor aus verändert werden. Kameraeinstellung ist derzeit auf einem DEFAULT Wert

--- Assets/Scripts/CameraPlayer2D.cs
+++ Assets/Scripts/CameraPlayer2D.cs
@@ -4,20 +4,27 @@ public class CameraPlayer2D : MonoBehaviour
 {
     public Transform target;
     public float lerpSpeed = 3;
+    public float clampMin;
+    public float clampMax;
 
     public Vector3 targetOffset;
     private Vector3 currPos;
-  
+
+
     private void FixedUpdate()
     {
-
+        float x = Mathf.Clamp(transform.position.x, clampMin, clampMax);
+        transform.position = new Vector3(x, currPos.y, targetOffset.z);
         if (target != null)
         {
+            
+
             currPos = target.position + targetOffset;
             //currPos.y = Mathf.Max(currPos.y, 8);
             Vector3 smooth = Vector3.Lerp(transform.position, currPos, lerpSpeed * Time.fixedDeltaTime);
             transform.position = smooth;
         }
+        
     }
 }
 
