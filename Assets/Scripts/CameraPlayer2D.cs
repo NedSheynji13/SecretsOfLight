3871547fc500d15e85e861b143c9abceff162673
NedSheynji13 <richardstrabl@gmail.com>
2018-03-27 14:18:09 +0200

    safety commit

--- Assets/Scripts/CameraPlayer2D.cs
+++ Assets/Scripts/CameraPlayer2D.cs
@@ -4,19 +4,32 @@ public class CameraPlayer2D : MonoBehaviour
 {
     public Transform target;
     public float lerpSpeed = 3;
+    public float clampMin;
+    public float clampMax;
 
-    public Vector2 targetOffset;
+    public Vector3 targetOffset;
     private Vector3 currPos;
-  
+
+
     private void FixedUpdate()
-    {   
-        if(target != null)
+    {
+        float x = Mathf.Clamp(transform.position.x, clampMin, clampMax);
+        transform.position = new Vector3(x, currPos.y, targetOffset.z);
+        if (target != null)
         {
+//<<<<<<< HEAD
 			currPos = target.position + new Vector3(0 , 0 , -10);
             currPos.y = Mathf.Max(currPos.y, 8);
+//=======
+            
+//>>>>>>> 9a3bd93e0a6f0d494028a83821fe02c7a33173d3
 
-            transform.position = Vector3.Lerp(transform.position, currPos, lerpSpeed * Time.deltaTime);
+            currPos = target.position + targetOffset;
+            //currPos.y = Mathf.Max(currPos.y, 8);
+            Vector3 smooth = Vector3.Lerp(transform.position, currPos, lerpSpeed * Time.fixedDeltaTime);
+            transform.position = smooth;
         }
+        
     }
 }
 
