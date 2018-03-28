9dc4ccd3d1aec4d3d00a48f195363ad72322fb60
NedSheynji13 <richardstrabl@gmail.com>
2018-03-28 18:38:39 +0200

    Reworked the Pause Menu using TextMesh Pro

---
+++ Assets/Scripts/TextChanger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextChanger : MonoBehaviour
+{
+    #region Variables
+    private TextMeshProUGUI Text;
+    public TMP_ColorGradient Default, hovered;
+    #endregion
+
+    void Start()
+    {
+        Text = gameObject.GetComponent<TextMeshProUGUI>();
+    }
+
+    public void Hovered()
+    {
+        Text.colorGradientPreset = hovered;
+    }
+
+    public void Exited()
+    {
+        Text.colorGradientPreset = Default;
+    }
+}
