4240893667f94d5e9affa1fefbcc96502046d8d4
Dominik Schraffl <dominik.schraffl@hotmail.com>
2018-03-28 16:39:34 +0200

    Added Pause Menu

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("TestScene");
+    }
+    public void CloseGame()
+    {
+        Debug.Log("Game Closed..");
+        //Application.Quit;
+    }
 }
